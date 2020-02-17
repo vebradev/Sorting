c00a4e82dd77c004920d16561e233067787a9af5
vebradev <justinas@vebra.dev>
2020-02-17 15:06:37 +0000

    implement bubble sorting and pass tests

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,8 +1,7 @@
-arr = [1, 9, 2, 8, 10, 3, 7, 4, 6, 5]
-
 # TO-DO: Complete the selection_sort() function below
 
-def selection_sort(arr = arr):
+
+def selection_sort(arr):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
         cur_index = i
@@ -23,8 +22,11 @@ def selection_sort(arr = arr):
 
 
 # TO-DO:  implement the Bubble Sort function below
-def bubble_sort(arr = arr):
-
+def bubble_sort(arr):
+    for i in range(0, len(arr) - 1):
+        for j in range(0, len(arr) - 1 - i):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
     return arr
 
 
